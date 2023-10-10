a7930dc71121d8a664054e3f35b738698a8deb4a
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-10-10 13:56:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-//your JS code here. If required.
+const cups = document.querySelectorAll('.cup-small');
+const remainedDiv = document.getElementById('remained');
+const percentageDiv = document.getElementById('percentage');
+
+let totalGlasses = 0;
+
+cups.forEach((cup, index) => {
+    cup.addEventListener('click', () => {
+        if (cup.classList.contains('full')) {
+            // If already full, remove full class and decrement total glasses
+            cup.classList.remove('full');
+            totalGlasses--;
+        } else {
+            // If not full, add full class and increment total glasses
+            cup.classList.add('full');
+            totalGlasses++;
+        }
+
+        // Update the remained and percentage divs
+        updateCupInfo();
+    });
+});
+
+function updateCupInfo() {
+    const goalLitres = 2;
+    const filledLitres = totalGlasses * 0.25; // Assuming each glass is 0.25 Litres
+
+    remainedDiv.textContent = `${goalLitres - filledLitres} Litres`;
+    percentageDiv.textContent = `${((filledLitres / goalLitres) * 100).toFixed(0)}%`;
+}
